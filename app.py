2239ad3d37061ba6556c60b56e9b0831d99f2391
maya x <maya.ex.klay@gmail.com>
2024-10-01 18:12:10 -0700

    milestone 2

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import chainlit as cl
 
+from agents.base_agent import Agent
+
 load_dotenv()
 
 # Note: If switching to LangSmith, uncomment the following, and replace @observe with @traceable
@@ -18,9 +20,32 @@ gen_kwargs = {
     "temperature": 0.2
 }
 
-SYSTEM_PROMPT = """\
-You are a pirate.
+PLANNING_PROMPT = """\
+You are a software architect, preparing to build the web page in the image. Generate a plan, \
+described below, in markdown format.
+
+In a section labeled "Overview", analyze the image, and describe the elements on the page, \
+their positions, and the layout of the major sections.
+
+Using vanilla HTML and CSS, discuss anything about the layout that might have different \
+options for implementation. Review pros/cons, and recommend a course of action.
+
+In a section labeled "Milestones", describe an ordered set of milestones for methodically \
+building the web page, so that errors can be detected and corrected early. Pay close attention \
+to the aligment of elements, and describe clear expectations in each milestone. Do not include \
+testing milestones, just implementation.
+
+Milestones should be formatted like this:
+
+ - [ ] 1. This is the first milestone
+ - [ ] 2. This is the second milestone
+ - [ ] 3. This is the third milestone
 """
+SYSTEM_PROMPT=PLANNING_PROMPT
+
+# Create an instance of the Agent class
+planning_agent = Agent(name="Planning Agent", client=client, prompt=PLANNING_PROMPT)
+
 
 @observe
 @cl.on_chat_start
